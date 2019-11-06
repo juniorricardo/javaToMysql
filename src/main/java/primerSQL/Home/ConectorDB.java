394c6dba977b0364935e7c074e6c232426c4edc6
Junior Ricardo <neo.junior.16@gmail.com>
2019-11-06 17:51:15 -0300

    Conexion con 'employees'.db establecidad >> JDBC

--- src/main/java/primerSQL/Home/ConectorDB.java
+++ src/main/java/primerSQL/Home/ConectorDB.java
@@ -2,32 +2,127 @@ package primerSQL.Home;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class ConectorDB {
 
-	private static String login = "root";
-	private static String password = "1234";
-	private static String url = "jdbc:mysql://localhost:3306/employees";
-	private static Connection connection_;
-	private static Statement st_ = null;
+	private String usuario;
+	private String contra;
+	private String url;
+	private Connection conexion;
+	private Statement st;
 
 	public ConectorDB() {
+		this.usuario = "root";
+		this.contra = "15903";
+		this.url = "jdbc:mysql://localhost:3306/employees";
+		this.st = null;
+	}
+
+	public ConectorDB(String login, String password, String url) {
+		super();
+		this.usuario = login;
+		this.contra = password;
+		this.url = url;
+		this.st = null;
+	}
+
+	public Connection testConexion() {
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			connection_ = DriverManager.getConnection(url, login, password);
-			if (connection_ != null) {
-				st_ = connection_.createStatement();
-				System.out.println("Conexion ok");
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conexion = DriverManager.getConnection(url, usuario, contra);
+			if (conexion != null) {
+				this.st = conexion.createStatement();
+				System.out.println("[Success] La conexion con la base de datos fue exitosa.");
 			} else
-				System.out.println("fail");
+				System.out.println("[Error] Ocurrio un problema con la conexion con la base de datos.");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		return conexion;
+	}
+
+
+	/**
+	 * {url}: de conexión para la base de datos mysql es 
+	 * 		>>>		jdbc:mysql://localhost:3306/employees 		<<< 
+	 * donde [jdbc] es la API, [mysql] es la base de datos, 
+	 * [localhost] es el nombre del servidor en el que se ejecuta mysql, 
+	 * tambien podemos usar la direccion IP , [3306] es el numero de puerto y
+	 * [employees] es el nombre de la base de datos. Podemos usar cualquier base de
+	 * datos, en tal caso, necesitamos reemplazar el sonoo con nuestro nombre de
+	 * base de datos.
+	 * 
+	 */ 
+	// Campos-> {employees.employees} [emp_no] [birth_date] [first_name] [last_name] [gender] [hire_date]
+	
+	/**
+	 * Recibe por parametro el nombre de la base de datos a establer conexion
+	 * @param db
+	 */
+	public void test2(String db) {
+		System.out.println("-------- MySQL JDBC Conexion Prueba ------------");
+		try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } 
+        catch (ClassNotFoundException e) {
+            System.out.println("MySQL JDBC Controlador no encontrado !!");
+            return;
+        }
+		System.out.println("MySQL JDBC Controlador detectado!");
+        Connection connect = null;
+		try {
+            connect = DriverManager.getConnection(
+            		"jdbc:mysql://localhost:3306/" + db, 
+            		"root", 
+            		"15903"
+            		);
+            System.out.println("[Success] Conexion SQL a la base de datos establecida!");
+            
+            /*
+             *  Consulta*******************************************************************************************
+             */
+            ResultSet resultadoQuery = st.executeQuery(
+						"select e.emp_no, e.first_name, e.last_name \r\n"+ 
+						"from employees as e\r\n" + 
+						"inner join titles as t\r\n" + 
+						"on e.emp_no = t.emp_no\r\n" + 
+						"where t.title= 'Engineer'\r\n" + 
+						"and e.last_name = 'Benzmuller';"
+						);
+            
+				System.out.println("ID\tNombre\t\tApellido");
+				while (resultadoQuery.next())
+					System.out.println(resultadoQuery.getInt(1) + "\t" + resultadoQuery.getString(2) + "\t\t"
+							+ resultadoQuery.getString(3));
+			//*****************************************************************************************************
+
+        } catch (SQLException e) {
+            System.out.println("[Error] La conexion fallo! Comprobar consola de salida");
+            return;
+        } 
+		
+		
+		/**
+		 * Cierra la conexion, exista o no una conexion establecida
+		 */
+		finally {
+            try
+            {
+                if(connect != null)
+                    connect.close();
+                System.out.println("Conexion cerrada !!");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+		
 	}
 }
--- src/main/java/primerSQL/Home/Home.java
+++ src/main/java/primerSQL/Home/Home.java
@@ -1,10 +1,18 @@
 package primerSQL.Home;
-
+/**
+ * 
+ * @author Cina
+ *
+ */
 public class Home {
 
+	
 	public static void main(String[] args) {
 
 		ConectorDB miConector = new ConectorDB();
+		miConector.testConexion();
+		miConector.test2("employees");
+		
 	}
 
 }
